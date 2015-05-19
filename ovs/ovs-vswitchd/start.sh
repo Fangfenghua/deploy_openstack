a917cb52f10f940ef31240f23fdf16827a58c6d2
Fang fenghua <449171342@qq.com>
2015-05-19 11:36:29 +0800

    Create start.sh

---
+++ ovs/ovs-vswitchd/start.sh
@@ -0,0 +1,4 @@
+
+#!/bin/bash
+
+ovs-vswitchd unix:/var/run/openvswitch/db.sock -vconsole:emer -vsyslog:err -vfile:info --mlockall --no-chdir --log-file=/var/log/openvswitch/ovs-vswitchd.log --pidfile=/var/run/openvswitch/ovs-vswitchd.pid
