4a18ce56bf7cb8d98e7af1b6bfb7d6c9df6dbad3
Fang fenghua <449171342@qq.com>
2015-05-19 15:32:47 +0800

    Update start.sh

--- ovs/ovs-vswitchd/start.sh
+++ ovs/ovs-vswitchd/start.sh
@@ -1,4 +1,9 @@
 
 #!/bin/bash
 
+UNIXSOCK=/var/run/openvswitch
+if test ! -e "$UNIXSOCK";then
+    mkdir $UNIXSOCK
+fi
+
 ovs-vswitchd unix:/var/run/openvswitch/db.sock -vconsole:emer -vsyslog:err -vfile:info --mlockall --no-chdir --log-file=/var/log/openvswitch/ovs-vswitchd.log --pidfile=/var/run/openvswitch/ovs-vswitchd.pid
